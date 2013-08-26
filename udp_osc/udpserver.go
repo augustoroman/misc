f45cf0707a7cb85a25f344d801ae3fcf4d04ab92
Augusto Roman <aroman@vannevartech.com>
2013-08-26 09:16:18 -0700

    use a separate goroutine for processing the messages to avoid dropped packets.

--- udp_osc/udpserver.go
+++ udp_osc/udpserver.go
@@ -9,6 +9,7 @@ import (
 	"github.com/gobs/cmd"
 	"log"
 	"net"
+	"runtime"
 	"strings"
 )
 
@@ -16,11 +17,21 @@ var port = flag.Int("port", 12345, "Port to listen on.")
 
 var msg, addr string
 
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
 func main() {
+	runtime.GOMAXPROCS(max(runtime.NumCPU(), 4))
+
 	flag.Parse()
 	fmt.Println("Welcome!")
 
 	server := newServer(*port)
+	go Process(server.Packets(), server.BufferReturn())
 	go server.run()
 
 	commander := &cmd.Cmd{
@@ -55,14 +66,51 @@ func main() {
 	commander.CmdLoop()
 }
 
+func Process(packets <-chan []byte, emptyreturn chan<- []byte) {
+	var md chunker.MessageDechunker
+	for data := range packets {
+		process(&md, data)
+		emptyreturn <- data[:cap(data)]
+	}
+}
+
+func process(md *chunker.MessageDechunker, data []byte) {
+	chunk, err := chunker.ParseChunk(data)
+	if err != nil {
+		log.Println("Malformed chunk: %v", err)
+		return
+	}
+
+	log.Printf("Message %d, Chunk %d/%d: %d bytes",
+		chunk.MessageId(), chunk.ChunkId(), chunk.NumChunks(),
+		len(chunk.Data()))
+
+	if md.AddChunk(chunk) {
+		msg := md.Assemble()
+		log.Printf("Got complete message of %d bytes\n", len(msg))
+		var parsed interface{}
+		err := json.Unmarshal([]byte(msg), &parsed)
+		if err != nil {
+			log.Printf("  [Failed to parse message to json: %v]", err)
+			log.Println("  Raw message: ", msg)
+		} else {
+			s, _ := json.MarshalIndent(parsed, "  ", "  ")
+			log.Println(string(s))
+		}
+	}
+
+}
+
 func parse(txt string) (addr, text string) {
 	splits := strings.SplitN(txt, " ", 2)
 	return splits[0], splits[1]
 }
 
 type UdpServer struct {
-	conn *net.UDPConn
-	hex  bool
+	databuffers chan []byte
+	conn        *net.UDPConn
+	hex         bool
+	packets     chan []byte
 }
 
 func (u *UdpServer) Dispatch(b *osc.Bundle) {
@@ -79,50 +127,51 @@ func newServer(port int) *UdpServer {
 		panic(err)
 	}
 
+	const NUM_BUFFERS = 1000
+	const BUFFER_SIZE = 64 << 10
+
+	buffers := make(chan []byte, NUM_BUFFERS)
+	for i := 0; i < NUM_BUFFERS; i++ {
+		buffers <- make([]byte, BUFFER_SIZE)
+	}
+
 	return &UdpServer{
-		conn: conn,
+		conn:        conn,
+		databuffers: buffers,
+		packets:     make(chan []byte, NUM_BUFFERS),
 	}
 }
 
 func (u *UdpServer) quit() {
+	close(u.packets)
 	u.conn.Close()
+}
+
+func (u *UdpServer) Packets() <-chan []byte {
+	return u.packets
+}
+
+func (u *UdpServer) BufferReturn() chan<- []byte {
+	return u.databuffers
 }
 
 func (u *UdpServer) run() {
 	log.Println("Listening on", u.conn.LocalAddr())
 	const maxPacketSize = 6 << 10 // 64k
-	data := make([]byte, maxPacketSize)
-	var md chunker.MessageDechunker
 	for {
-		msglen, from, err := u.conn.ReadFromUDP(data)
+		data := <-u.databuffers
+		msglen, _, err := u.conn.ReadFromUDP(data)
 		if err != nil {
 			log.Println("Udp error: ", err)
 			break
 		}
-
-		chunk, err := chunker.ParseChunk(data[:msglen])
-		if err != nil {
-			log.Println("Malformed chunk: %v", err)
-			continue
-		}
-
-		log.Printf("Message %d, Chunk %d/%d: %d bytes from %v",
-			chunk.MessageId(), chunk.ChunkId(), chunk.NumChunks(),
-			len(chunk.Data()), from)
-
-		if md.AddChunk(chunk) {
-			msg := md.Assemble()
-			log.Printf("Got complete message of %d bytes\n", len(msg))
-			var parsed interface{}
-			err := json.Unmarshal([]byte(msg), &parsed)
-			if err != nil {
-				log.Printf("  [Failed to parse message to json: %v]", err)
-			} else {
-				s, _ := json.MarshalIndent(parsed, "  ", "  ")
-				log.Println(string(s))
-			}
-		}
+		u.packets <- data[:msglen]
 	}
+	log.Println("Udp server died.")
+}
+
+func (u *UdpServer) release(buffer []byte) {
+	u.databuffers <- buffer
 }
 
 func (u *UdpServer) Send(addr, text string) {
